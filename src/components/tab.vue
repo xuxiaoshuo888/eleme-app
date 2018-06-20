e66b2976fdd48d0c2c6bd71e1a91674e9f791b88
xuxiaoshuo888 <754124808@qq.com>
2018-06-20 08:29:47 +0800

    添加了一个组件

---
+++ src/components/tab.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="tab">
+      <div class="tab_son">首页</div>
+      <div class="tab_son">发现</div>
+      <div class="tab_son">订单</div>
+      <div class="tab_son">我的</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "tab"
+    }
+</script>
+
+<style scoped>
+  .tab{
+    display: flex;
+    flex-direction: row;
+  }
+  .tab_son{
+    flex-grow: 1;
+  }
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>123123</div>
+    <Tab></Tab>
+  </div>
+
+</template>
+
+<script>
+    import Tab from "../components/tab";
+    export default {
+        name: "home",
+      component: {Tab}
+    }
+</script>
+
+<style scoped>
+
+</style>
